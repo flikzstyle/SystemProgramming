2bf1d8977f75c0d7efa65620929698cab1a0629c
flikzstyle <flikz@DESKTOP-8QAQEB3.localdomain>
2025-09-18 23:38:53 +0300

    lab1

---
+++ lab1/sandbox/C_version.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Yakushev\n");
+    printf("Pavel\n");
+    printf("Alekseevich\n");
+    return 0;
+}
---
+++ version.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("АНДРЕЙ ПИДОРЕБАНЫЙ\n");
+    return 0;
+}
