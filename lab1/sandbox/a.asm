1de58c7f44fc48a59bc7bc5715bca50243d54060
flikzstyle <flikz@DESKTOP-8QAQEB3.localdomain>
2025-09-26 09:49:24 +0300

    fix

--- lab1/sandbox/a.asm
+++ lab1/sandbox/a.asm
@@ -3,14 +3,12 @@ public _start
 msg db "Yakushev", 0xA, "Pavel", 0xA, "Alekseevich", 0xA, 0
 
 _start:
-    ;инициализация регистров для вывода информации на экран
     mov rax, 4
     mov rbx, 1
     mov rcx, msg
-    mov rdx, 29
+    mov rdx, 28
     int 0x80
 
-    ;инициализация регистров для успешного завершения работы программы
     mov rax, 1
     mov rbx, 0
     int 0x80
